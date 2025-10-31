42c03e45f976f6e53aa109e277abcd9c36b19131
veltays <yassine.gran@student.hepl.be>
2025-10-31 10:50:12 +0100

    Restructuration des fichier lunix

--- Linux/Etape1.3/build/CMakeFiles/serveur_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/serveur_autogen"
-  "serveur_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/serveur_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
